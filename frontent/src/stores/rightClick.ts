cc530eb312a35a0123799056b2c5e66e4c4524de
higashi_2500 <walkurelulu@qq.com>
2025-01-09 17:18:49 +0900

    完善部分side.vue

--- frontent/src/stores/rightClick.ts
+++ frontent/src/stores/rightClick.ts
@@ -6,7 +6,6 @@ export const useRightClickStore = defineStore('rightClick', () => {
     const isRightChickActive = ref(false)
 
     const menuPosition = ref({ x: 0, y: 0 })
-
     let debounceTimeout: number | null = null
 
     const toggleRightClick = (event: MouseEvent) => {
@@ -17,7 +16,7 @@ export const useRightClickStore = defineStore('rightClick', () => {
 
         isRightChickActive.value = false
 
-        
+
         debounceTimeout = window.setTimeout(() => {
             isRightChickActive.value = true
             menuPosition.value = { x: event.clientX, y: event.clientY }
