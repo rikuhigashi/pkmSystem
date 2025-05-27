ebe4f2a0fa60fa2b01b0961f2e027f7d88651961
higashi_2500 <walkurelulu@qq.com>
2025-05-27 12:19:45 +0900

    修改JwtAuthenticationFilter以使用电子邮件从JWT令牌中获取用户名

--- frontent/src/API/axios.ts
+++ frontent/src/API/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, // 动态读取 baseURL
 
-  // withCredentials: true,
+  withCredentials: true,
   timeout: 5000, // 请求超时时间
   headers: {
     'Content-Type': 'application/json',
